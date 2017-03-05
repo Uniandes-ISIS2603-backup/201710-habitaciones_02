6a5eb7745c2de4cedf8ac0c32bf27d8993a45200
SANTIAGO CORTES FERNANDEZ <s.cortes@sis.virtual.uniandes.edu.co>
2017-03-05 12:12:44 -0500

    clase abstracta del usuario

---
+++ habitaciones-logic/src/main/java/co/edu/uniandes/csw/habitaciones/entities/ViajeroEntity.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.habitaciones.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+/**
+ *
+ * @author s.cortes
+ */
+@Entity
+@PrimaryKeyJoinColumn(referencedColumnName="idUsuario")
+public class ViajeroEntity extends UsuarioEntity
+{
+    //----------------------------------------------------------------------------------------------------
+    // ATRIBUTOS
+    //----------------------------------------------------------------------------------------------------
+
+    /**
+     * Lista con las reservas del viajero
+     */
+    
+    //----------------------------------------------------------------------------------------------------
+    // METODOS
+    //----------------------------------------------------------------------------------------------------
+
+}
