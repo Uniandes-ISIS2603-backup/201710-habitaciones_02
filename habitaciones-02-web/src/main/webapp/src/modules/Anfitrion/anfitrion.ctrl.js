74bca39116acc373e1b03b16a2fe81a2cd650484
df.sanabria761 <df.sanabria761@ISIS2603-0140.sis.virtual.uniandes.edu.co>
2017-03-29 10:25:23 -0500

    Vista lista del anfitrión completa

--- habitaciones-02-web/src/main/webapp/src/modules/Anfitrion/anfitrion.ctrl.js
+++ habitaciones-02-web/src/main/webapp/src/modules/Anfitrion/anfitrion.ctrl.js
@@ -1,1 +1,9 @@
-Anfitrion ctrls
+(function(ng) {
+    var mod = ng.module("anfitrionModule");
+    
+    mod.controller("anfitrionListCtrl", ['$scope','anfitriones',
+        function($scope,anfitriones) {
+            $scope.anfitrionesRecords = anfitriones.data;
+        }
+    ]);
+})(window.angular);
