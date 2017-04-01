851140fba04f1bbc226d1e05e95e38462202e4ce
ne.cabrera <ne.cabrera@ISIS2603-0157.sis.virtual.uniandes.edu.co>
2017-04-01 01:05:20 -0500

    Merge origin/master
    Conflicts:
    	habitaciones-02-web/src/main/webapp/index.html
    	habitaciones-02-web/src/main/webapp/src/app.js

--- habitaciones-02-web/src/main/webapp/src/modules/Pago/pago.ctrl.js
+++ habitaciones-02-web/src/main/webapp/src/modules/Pago/pago.ctrl.js
@@ -1,1 +1,9 @@
-pago ctrls
+(function(ng) {
+    var mod = ng.module("pagoModule");
+    
+    mod.controller("pagoListCtrl", ['$scope','pagos',
+        function($scope,pagos) {
+            $scope.pagosRecords = pagos.data;
+        }
+    ]);
+})(window.angular);
--- habitaciones-02-web/src/main/webapp/src/modules/Pago/pago.mod.js
+++ habitaciones-02-web/src/main/webapp/src/modules/Pago/pago.mod.js
@@ -1,32 +1,36 @@
 (function (ng) {
-    // Definición del módulo
     var mod = ng.module("pagoModule", ['ui.router']);
- 
-   // Configuración de los estados del módulo
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            // En basePath se encuentran los templates y controladores de módulo
-            var basePath = 'src/modules/pagos/';
-            // Mostrar la lista de libros será el estado por defecto del módulo
+
+            var basePath = 'src/modules/Pago/';
             $urlRouterProvider.otherwise("/pagosList");
-            // Definición del estado 'pagosList' donde se listan los libros
-            $stateProvider.state('pagosList', {
-                // Url que aparecerá en el browser
-                url: '/pagos/list',
-                // Se define una variable pagos (del estado) que toma por valor 
-                // la colección de libros que obtiene utilizando $http.get 
-                 resolve: {
+            $stateProvider.state('pagos', {
+                url: '/pagos',
+                abstract: true,
+                resolve: {
                     pagos: ['$http', function ($http) {
-                            return $http.get('data/pagos.json'); // $http retorna un apromesa que aquí no se está manejando si viene con error.
+                            return $http.get('data/pagos.json');
                         }]
                 },
-                // Template que se utilizara para ejecutar el estado
-                templateUrl: basePath + 'pagos.list.html',
-                // El controlador guarda en el scope en la variable pagosRecords los datos que trajo el resolve
-                // pagosRecords será visible en el template
-                controller: ['$scope', 'pagos', function ($scope, pagos) {
-                        $scope.pagosRecords = pagos.data;
-                    }]              
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'pago.html',
+                        controller: ['$scope', 'pagos', function ($scope, pagos) {
+                                $scope.pagosRecords = pagos.data;
+                            }]
+                    }
+                }
+            }).state('pagosList',{
+                url: '/list',
+                parent: 'pagos',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'pago.list.html'
+                        
+                    }
+                }
             });
-        }
-    ]);
-})(window.angular);
+        }]
+    );
+}
+)(window.angular);
