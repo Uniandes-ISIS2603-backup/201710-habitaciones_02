e35d9d7c09fb348f2934407757613b9fbd718eba
df.sanabria761 <df.sanabria761@ISIS2603-0140.sis.virtual.uniandes.edu.co>
2017-04-01 20:47:31 -0500

    Carrusel terminado finalmente, cambia el detail también.

--- habitaciones-02-web/src/main/webapp/src/modules/Anfitrion/anfitrion.ctrl.js
+++ habitaciones-02-web/src/main/webapp/src/modules/Anfitrion/anfitrion.ctrl.js
@@ -1,10 +1,17 @@
 (function(ng) {
     var mod = ng.module("anfitrionModule");
-    
     mod.controller("anfitrionListCtrl", ['$scope','anfitriones',
         function($scope,anfitriones) {
             $scope.anfitrionesRecords = anfitriones.data;
         }
     ]);
-    
+    mod.controller("anfitrionCarouselCtrl",['$scope','anfitriones','anfitrionActual', 'anfitrionSiguiente','anfitrionAnterior', 'anfitrionPresente',
+        function($scope,anfitriones,anfitrionActual, anfitrionSiguiente, anfitrionAnterior, anfitrionPresente){
+            $scope.anfitrionPresente = anfitrionPresente;
+            $scope.anfitrionesRecords = anfitriones.data;
+            $scope.anfitrionActual=anfitrionActual;
+            $scope.anfitrionAnterior=anfitrionAnterior;
+            $scope.anfitrionSiguiente=anfitrionSiguiente;
+           
+                    }]);
 })(window.angular);
