7197f68e651b7a8a3c68a6959465de71b7ac167c
SANTIAGO CORTES FERNANDEZ <s.cortes@sis.virtual.uniandes.edu.co>
2017-04-02 20:56:37 -0500

    cambios en los recursos de habitacion, resena y viajero

--- habitaciones-02-web/src/main/java/co/edu/uniandes/csw/habitaciones/resources/HabitacionResource.java
+++ habitaciones-02-web/src/main/java/co/edu/uniandes/csw/habitaciones/resources/HabitacionResource.java
@@ -87,18 +87,6 @@ public class HabitacionResource {
         }
         return new HabitacionDetailDTO(habitacionLogic.getHabitacion(id));
     }
-    
-    /**
-     * Metodo encargado de llamar a la clase ResenaResourse para retornar las
-     * resenas de una habitacion respectiva
-     * @return la clase ResenaResourse encargada de retornar las resenas de
-     * la habitacion.
-     */
-    @Path("{habitacionId: \\d+}/resenas")
-    public Class<ResenaResource> getResenasHabitacion()
-    {
-        return  ResenaResource.class;
-    }
 
     /**
      * Crea una nueva habitacion
